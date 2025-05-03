725183b355f9fd81ffd39872fa1cd78be896ad54
Hassan-K-Ayoub <hassan.kh.ayoub@hotmail.com>
2025-05-03 10:44:45 +0300

    Added users seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $users = [
+            ['name' => 'Ziad Awad', 'email' => 'contact@awadsrealestate.com', 'password' => '$2y$12$DSuXjPD0kNlOsnZ20eSS3.IsqcoL4siA6avax8pgy6/VafFaacnku'],
+        ];
+
+        DB::table('users')->insert($users);
+
+        $this->command->info('Successfully seeded users!');
+    }
+}
